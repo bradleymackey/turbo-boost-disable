0fb2d80380e590058f4fa60e9fd84dcfc1421ec8
Bradley Mackey <bradleymackey@me.com>
2020-02-11 11:40:13 +0000

    fix script

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 # runs a full cycle for ensuring that turbo-boost is in fact disabled
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-$DIR/unload.sh
-$DIR/load.sh
+sudo $DIR/unload.sh
+sudo $DIR/load.sh
